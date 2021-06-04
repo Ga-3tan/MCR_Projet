45c1d27492674d86e4d89d1941d0f7f1e8f57024
MazieroMarco <maziero.marco@gmail.com>
2021-06-04 22:58:07 +0200

    Added game window elements and layouts

--- src/main/java/SpaceShooter.java
+++ src/main/java/SpaceShooter.java
@@ -1,12 +1,11 @@
+import gameEngine.Controller;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SpaceShooter {
-
-
     public static void main(String[] args) {
-
+        Controller.getInstance();
     }
 }
